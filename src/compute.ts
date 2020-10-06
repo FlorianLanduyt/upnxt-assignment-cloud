5058038e4aea42f562c0d0b6e3f43721884e6755
Florian Landuyt <florian.landuyt.y2414@student.hogent.be>
2020-10-06 14:10:27 +0200

    Update comments to functions

--- src/compute.ts
+++ src/compute.ts
@@ -5,15 +5,17 @@ export function compute(game: Game): number {
   var totalScore: number = 0
 
   for (var frameIndex = 0; frameIndex < game.length; frameIndex++) {
+    //compute score for first 9 frames
     if(frameIndex < game.length -2)
       totalScore = getScoreForNormalFrame(game, frameIndex, totalScore);
+    //compute score for last frame
     else 
       totalScore = getScoreForLastFrame(game, totalScore)
   }
   return totalScore
 }
 
-
+//Get the score from the first 9 frames
 function getScoreForNormalFrame(game: Game, frameIndex: number, totalScore: number) {
   var currentFrame = game[frameIndex]
 
@@ -28,6 +30,7 @@ function getScoreForNormalFrame(game: Game, frameIndex: number, totalScore: numb
   return totalScore;
 }
 
+//Get the score from the 10th frame
 function getScoreForLastFrame(game: Game, totalScore: number){
   var lastFrame = game[game.length-1]
 
@@ -38,10 +41,12 @@ function getScoreForLastFrame(game: Game, totalScore: number){
   return totalScore
 }
 
+//Checks if the frame has a strike
 function checkForStrike(frame: Frame|LastFrame): boolean{
   return frame[0] == 10? true: false 
 }
 
+// Gets the bonus for a strike (= + score of next two rolls)
 function getStrikeBonus(game: Game, frame: number): number {
   var bonus = 0
   var roll1 = game[frame][0]
@@ -54,10 +59,12 @@ function getStrikeBonus(game: Game, frame: number): number {
   return bonus
 }
 
+//Checks if the frame has a spare
 function checkForSpare(frame: Frame|LastFrame): boolean{
   return (frame[0] + frame[1] == 10) && frame[0] != 10 ? true: false
 }
 
+//Gets the bonus for a spare (+ score of next roll)
 function getSpareBonus(frame: Frame|LastFrame): number {
   return frame[0]
 }
