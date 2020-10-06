da5d2847717b89dec72c7def4786036a92929547
Florian Landuyt <florian.landuyt.y2414@student.hogent.be>
2020-10-06 14:11:26 +0200

    Added tests for validation of the input

--- tests/server.test.ts
+++ tests/server.test.ts
@@ -21,7 +21,199 @@ it("has a compute endpoint that returns the score with status code 200", async (
     });
 
   expect(response.status).toBe(200);
+  expect(response.body.score).toBe(300);
+});
 
-  // In comment because with the ID the body is always different 
-  //expect(response.body).toEqual({ score: 300 });
+it("returns a 400 response code when entering more than 10 pins in 1 FRAME ", async () => {
+  const server = createServer();
+  const response = await supertest(server)
+    .post("/compute")
+    .send({
+      game: [
+        [10, 10],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0, 0],
+      ],
+    });
+
+  expect(response.status).toBe(400);
+});
+
+it("returns a 400 response code when entering more than 10 pins in 1 ROLL ", async () => {
+  const server = createServer();
+  const response = await supertest(server)
+    .post("/compute")
+    .send({
+      game: [
+        [11, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0, 0],
+      ],
+    });
+
+  expect(response.status).toBe(400);
+});
+
+it("returns a 400 response code when rolling a third time whithout getting a strike or spare in last frame ", async () => {
+  const server = createServer();
+  const response = await supertest(server)
+    .post("/compute")
+    .send({
+      game: [
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0, 6],
+      ],
+    });
+
+  expect(response.status).toBe(400);
+});
+
+it("returns a 400 response code when entering more than 10 frames", async () => {
+  const server = createServer();
+  const response = await supertest(server)
+    .post("/compute")
+    .send({
+      game: [
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0, 0],
+      ],
+    });
+
+  expect(response.status).toBe(400);
+});
+
+it("returns a 400 response code when entering less than 10 frames", async () => {
+  const server = createServer();
+  const response = await supertest(server)
+    .post("/compute")
+    .send({
+      game: [
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0, 0],
+      ],
+    });
+
+  expect(response.status).toBe(400);
+});
+
+it("returns a 400 response code when entering more than 2 rolls in one frame in the first 9 frames", async () => {
+  const server = createServer();
+  const response = await supertest(server)
+    .post("/compute")
+    .send({
+      game: [
+        [0, 0],
+        [0,0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0, 0],
+      ],
+    });
+
+  expect(response.status).toBe(400);
+});
+
+it("returns a 400 response code when entering less than 2 rolls in one frame in the first 9 frames", async () => {
+  const server = createServer();
+  const response = await supertest(server)
+    .post("/compute")
+    .send({
+      game: [
+        [0, 0],
+        [0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0, 0],
+      ],
+    });
+
+  expect(response.status).toBe(400);
+});
+
+it("returns a 400 response code when entering less than 3 rolls in one frame in the 10th frame", async () => {
+  const server = createServer();
+  const response = await supertest(server)
+    .post("/compute")
+    .send({
+      game: [
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+      ],
+    });
+
+  expect(response.status).toBe(400);
+});
+
+it("returns a 400 response code when entering more than 3 rolls in one frame in the 10th frame", async () => {
+  const server = createServer();
+  const response = await supertest(server)
+    .post("/compute")
+    .send({
+      game: [
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0, 0, 0],
+      ],
+    });
+
+  expect(response.status).toBe(400);
 });
